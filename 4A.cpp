597e645ac51687e1780bbafbfbcc2162a70ba973
jassibhullar <jashanbhullar376@gmail.com>
2017-08-14 03:23:28 +0530

    Arya and Bran

---
+++ 4A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int int main(int argc, char const *argv[])
+{
+	int n;
+	cin >> n;
+	cout << (n%2==0 && n!=2)?"YES":"NO";
+
+	return 0;
+}
---
+++ 839A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k;
+	cin >> n >> k;
+	int a;
+	int arya, bran, tmp;
+	
+	arya = bran = tmp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		arya += a;
+
+		if(arya > 8) tmp = 8;
+		else tmp = arya;
+
+		arya -= tmp;
+		bran += tmp;
+
+		if(bran >= k){
+			cout << i+1;
+			return 0;
+		}
+	}
+
+	cout << -1;
+
+	return 0;
+}
