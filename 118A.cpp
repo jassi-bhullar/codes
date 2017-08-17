cfc2ac21f16c58c75fcb277b001f8648ebcf9bcc
jassibhullar <jashanbhullar376@gmail.com>
2017-08-17 16:54:42 +0530

    string

---
+++ 118A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+
+	int len = s.length();
+	string w="";
+
+	int t=len;
+	int i=0;
+	while(t--){
+		char c = s[i];
+		i++;
+		c = tolower(c);
+		if(c=='a' || c=='y' || c=='u' || c=='o' || c=='i' || c=='e') continue;
+
+		w += ".";
+		w += c;
+		
+	}
+
+	cout << w;
+
+	return 0;
+}
